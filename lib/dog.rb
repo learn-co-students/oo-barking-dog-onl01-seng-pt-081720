129bb94e6973cf8b509b79a620fdd943c5093561
Jazmin Matos <jmatos1993@gmail.com>
2020-09-01 03:09:25 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Dog 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+  
+  def name 
+    @this_dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
+
+#need name setter
+#need name getter
+#create bark instance method 
